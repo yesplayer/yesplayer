d3ed188408c93badb7b6555ca12114686989e353
Nemanja Nedeljković <nemanjan00@gmail.com>
2017-10-16 13:11:59 +0200

    Update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const audio = require("./audio")();
 
-audio.addTrack("http://localhost:3000/download/gb-B3lsgEfA");
+audio.addTrack("https://party.nemanja.top/download/gb-B3lsgEfA");
 
 const graph = require("./graph");
 
