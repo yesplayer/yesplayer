6f5443c781b4da002e5d639e5ad97d0c38c7252b
Nemanja Nedeljković <nemanjan00@gmail.com>
2017-11-14 01:47:06 +0100

    GUI

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
 	entry: './src/app.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.bundle.js'
+		filename: 'main.bundle.js',
+		publicPath: '../dist/'
 	},
 	module: {
 		rules: [
