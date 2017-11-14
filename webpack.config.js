d2b5cc54f5a380da0eb3ad5c09a7d6cee3e45cc5
Nemanja Nedeljković <nemanjan00@gmail.com>
2017-11-14 01:47:49 +0100

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,17 @@ module.exports = {
 		rules: [
 			{
 				test: /\.scss$/,
-				use: [{
-					loader: "style-loader" // creates style nodes from JS strings
-				}, {
-					loader: "css-loader" // translates CSS into CommonJS
-				}, {
-					loader: "sass-loader" // compiles Sass to CSS
-				}]
+				use: [
+					{
+						loader: "style-loader" // creates style nodes from JS strings
+					},
+					{
+						loader: "css-loader" // translates CSS into CommonJS
+					},
+					{
+						loader: "sass-loader" // compiles Sass to CSS
+					}
+				]
 			},
 			{
 				test: /\.css$/,
